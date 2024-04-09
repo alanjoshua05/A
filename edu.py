69563d46e64281c9e06738dab305b5d19e12fc03
alanjoshua05 <155869499+alanjoshua05@users.noreply.github.com>
2024-04-09 17:01:13 +0530

    Update edu.py

--- edu.py
+++ edu.py
@@ -3,6 +3,10 @@ from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
 from datetime import datetime, time
 import json
+from streamlit_option_menu import option_menu
+import webbrowser
+import sys
+from io import StringIO
 
 scopes = ['https://www.googleapis.com/auth/spreadsheets',
           "https://www.googleapis.com/auth/drive"]
@@ -19,6 +23,33 @@ with open('question.json','r') as file:
     # Parse the JSON data
     data = json.loads(json_data)
 
+def python():
+    st.title('Python IDE')
+
+# Define initial code
+
+    initial_code = '''print("Hello World")'''
+
+# Display the code editor
+    code = st.text_area(f"1){data['Code question']}", value=initial_code, height=300)
+
+# Button to execute the code
+    if st.button('Run'):
+    # Redirect stdout to capture output
+        output_area = st.empty()
+        stdout = sys.stdout
+        sys.stdout = StringIO()
+    
+    # Execute the code
+        try:
+            exec(code)
+            output_value = sys.stdout.getvalue()
+            output_area.write(output_value)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+    
+    # Restore stdout
+        sys.stdout = stdout
 
 
 def is_blocked_time():
@@ -27,30 +58,57 @@ def is_blocked_time():
     now = datetime.now().time()
     return blocked_start_time < now < blocked_end_time
 
-
-# Streamlit app
 current_date = datetime.now().date()
-st.title('Daily quiz')
-st.write(current_date.strftime('%d/%m/%y'))
+# Streamlit app
+with st.sidebar:
+     selected = option_menu(
+          menu_title="1st Year CSBS",
+          options=["Daily quiz","Coding practice","PYQs"]
+     )
+if selected == "Daily quiz":
+    
+    st.title('Daily quiz')
+    st.write(current_date.strftime('%d/%m/%y'))
 
 # Get input from the user
-name = st.text_input('Enter Name')
-email = st.text_input('Enter your college mail id (ex: 23cb000@drngpit.ac.in)')
-email = email.strip()
+    name = st.text_input('Enter Name')
+    email = st.text_input('Enter your college mail id (ex: 23cb000@drngpit.ac.in)')
+    email = email.strip()
 
-st.subheader("Questions")
+    st.subheader("Questions")
 
-q1 = st.text_area(f"1){data['Question 1']}")
+    q1 = st.text_area(f"1){data['Question 1']}")
 
 
-if st.button("Submit"):
-    if "@drngpit.ac.in" not in email or len(email) > 21:
-        st.error("Enter your college mail id correctly")
-    elif is_blocked_time():
-        st.error("Sorry, the quiz has been timed out.")
-    else:
+    if st.button("Submit",type='primary'):
+        if "@drngpit.ac.in" not in email or len(email) > 21:
+            st.error("Enter your college mail id correctly")
+        elif is_blocked_time():
+            st.error("Sorry, the quiz has been timed out.")
+        else:
     
             # Update the sheet with the entered data
             sheet.append_row([current_date.strftime('%Y-%m-%d'), name, email, q1])
             st.success("Added successfully")
             st.balloons()
+
+if selected == "PYQs":
+    st.title("Previous year questions")
+    st.subheader("Internal 1")
+    if st.button('Engineering Economics with Applications'):
+        webbrowser.open("#")
+    if st.button('Engineering Grapics'):
+        webbrowser.open("#")
+    if st.button('Python Programming'):
+        webbrowser.open("#")
+    if st.button('Technical English'):
+        webbrowser.open("#")
+    if st.button('Linear Algebra'):
+        webbrowser.open("#")
+    if st.button('Physics for Information Science'):
+        webbrowser.open("#")
+    if st.button('Digital Principles and System Design'):
+        webbrowser.open("#")
+
+if selected=="Coding practice":
+    python()

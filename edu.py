8ad41cfaf5ff3287657508bd4e57a720bd32e987
alanjoshua05 <155869499+alanjoshua05@users.noreply.github.com>
2024-05-27 00:15:22 +0530

    Update edu.py

--- edu.py
+++ edu.py
@@ -8,6 +8,9 @@ import webbrowser
 import sys
 from io import StringIO
 
+st.sidebar.title("Daily Quiz")
+a = st.sidebar.selectbox("Week 1",['Day 1','Day 2','Day 3','Day 4','Day 5'])
+
 scopes = ['https://www.googleapis.com/auth/spreadsheets',
           "https://www.googleapis.com/auth/drive"]
 
@@ -23,35 +26,6 @@ with open('question.json','r') as file:
     # Parse the JSON data
     data = json.loads(json_data)
 
-def python():
-    st.title('Python IDE')
-
-# Define initial code
-
-    initial_code = '''print("Hello World")'''
-
-# Display the code editor
-    code = st.text_area(f"1){data['Code question']}", value=initial_code, height=300)
-
-# Button to execute the code
-    if st.button('Run'):
-    # Redirect stdout to capture output
-        output_area = st.empty()
-        stdout = sys.stdout
-        sys.stdout = StringIO()
-    
-    # Execute the code
-        try:
-            exec(code)
-            output_value = sys.stdout.getvalue()
-            output_area.write(output_value)
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
-    
-    # Restore stdout
-        sys.stdout = stdout
-
-
 def is_blocked_time():
     blocked_start_time = time(19,30)  # e.g., 12:00 AM 18:30
     blocked_end_time = time(23,00)    # e.g., 4:30 AM 23:00
@@ -59,17 +33,12 @@ def is_blocked_time():
     return blocked_start_time < now < blocked_end_time
 
 current_date = datetime.now().date()
-# Streamlit app
-with st.sidebar:
-     selected = option_menu(
-          menu_title="1st Year CSBS",
-          options=["Daily quiz","Coding practice","Internal 1"]
-     )
-if selected == "Daily quiz":
-    
-    st.title('Daily Quiz')
-    st.write(current_date.strftime('%d/%m/%y'))
 
+    
+if a == 'Day 1':
+    
+    st.title('Daily quiz')
+    st.subheader(f"Date : {data['Day_1']['Date']}")
 # Get input from the user
     name = st.text_input('Enter Name')
     email = st.text_input('Enter your college mail id (ex: 23cb000@drngpit.ac.in)')
@@ -77,39 +46,69 @@ if selected == "Daily quiz":
 
     st.subheader("Questions")
 
-    q1 = st.text_area(f"1){data['Question 1']}")
-    st.info("This has been temporarily paused for improvement and optimization")
+    q1 = st.text_area(f"1){data['Day_1']['Question']}")
 
-    # if st.button("Submit",type='primary'):
-    #     if "@drngpit.ac.in" not in email or len(email) > 21:
-    #         st.error("Enter your college mail id correctly")
-    #     elif is_blocked_time():
-    #         st.error("Sorry, the quiz has been timed out.")
-    #     else:
+
+    if st.button("Submit",type='primary'):
+        if "@drngpit.ac.in" not in email or len(email) != 21:
+            st.error("Enter your college mail id correctly")
+        elif is_blocked_time():
+            st.error("Sorry, the quiz has been timed out.")
+        else:
     
-    #         # Update the sheet with the entered data
-    #         sheet.append_row([current_date.strftime('%Y-%m-%d'), name, email, q1])
-    #         st.success("Added successfully")
-    #         st.balloons()
+            # Update the sheet with the entered data
+            sheet.append_row(["2024-05-17", name, email, q1])
+            st.success("Added successfully")
+            st.balloons()
 
-if selected == "Internal 1":
-    st.title("Previous questions")
-    st.subheader("Internal 1")
-    if st.button('Engineering Economics with Applications'):
-        st.markdown("[Engineering Economics with Applications](https://drive.google.com/file/d/1J8ZWpcczv39NRvD1T-SY7K1LubqymIBl/view)")
-    if st.button('Python Programming'):
-        st.info("This will be updated soon")
-        # st.markdown("[Python Programming](#)")
-    if st.button('Technical English'):
-        st.markdown("[Technical English](https://drive.google.com/file/d/1JT4JYF2w0gFV5DgUGyQJzGqC3j4rVZfs/view?usp=sharing)")
-    if st.button('Linear Algebra'):
-        # st.markdown("[Linear Algebra](#)")
-        st.info("This will be updated soon")
-    if st.button('Physics for Information Science'):
-        # st.markdown("[Physics for Information Science](#)")
-        st.info("This will be updated soon")
-    if st.button('Digital Principles and System Design'):
-        st.markdown("[Digital Principles and System Design](https://drive.google.com/file/d/1JJoTvviMBcwVmb2IZt1tPbQpSMYOlkCw/view?usp=sharing)")
+if a == 'Day 2':
+    
+    st.title('Daily quiz')
+    st.subheader(f"Date : {data['Day_2']['Date']}")
+# Get input from the user
+    name = st.text_input('Enter Name')
+    email = st.text_input('Enter your college mail id (ex: 23cb000@drngpit.ac.in)')
+    email = email.strip()
 
-if selected=="Coding practice":
-    python()
+    st.subheader("Questions")
+
+    q1 = st.text_area(f"1){data['Day_2']['Question']}")
+
+
+    if st.button("Submit",type='primary'):
+        if "@drngpit.ac.in" not in email or len(email) != 21:
+            st.error("Enter your college mail id correctly")
+        elif is_blocked_time():
+            st.error("Sorry, the quiz has been timed out.")
+        else:
+    
+            # Update the sheet with the entered data
+            sheet.append_row(["2024-05-17", name, email, q1])
+            st.success("Added successfully")
+            st.balloons()
+
+if a == 'Day 3':
+    
+    st.title('Daily quiz')
+    st.subheader(f"Date : {data['Day_3']['Date']}")
+# Get input from the user
+    name = st.text_input('Enter Name')
+    email = st.text_input('Enter your college mail id (ex: 23cb000@drngpit.ac.in)')
+    email = email.strip()
+
+    st.subheader("Questions")
+
+    q1 = st.text_area(f"1){data['Day_3']['Question']}")
+
+
+    if st.button("Submit",type='primary'):
+        if "@drngpit.ac.in" not in email or len(email) != 21:
+            st.error("Enter your college mail id correctly")
+        elif is_blocked_time():
+            st.error("Sorry, the quiz has been timed out.")
+        else:
+    
+            # Update the sheet with the entered data
+            sheet.append_row([data['Day_3']['Date'], name, email, q1])
+            st.success("Added successfully")
+            st.balloons()
